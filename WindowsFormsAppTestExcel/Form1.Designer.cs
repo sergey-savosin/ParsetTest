9f6800a868ea2bb1791c70be19d49461a6c94523
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2018-09-20 22:06:23 +0300

    Add project files.

---
+++ WindowsFormsAppTestExcel/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsAppTestExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxAddinPath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxExcelSheetPath = new System.Windows.Forms.TextBox();
+            this.btnSelectExcelFile = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.btnParserTest = new System.Windows.Forms.Button();
+            this.openExcelFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxActiveParserName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(12, 173);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(197, 37);
+            this.btnRun.TabIndex = 5;
+            this.btnRun.Text = "Запуск надстройки";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Пример запуска надстройки Парсер.";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(15, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(552, 1);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Путь к надстройке";
+            // 
+            // textBoxAddinPath
+            // 
+            this.textBoxAddinPath.Location = new System.Drawing.Point(12, 59);
+            this.textBoxAddinPath.Name = "textBoxAddinPath";
+            this.textBoxAddinPath.Size = new System.Drawing.Size(491, 20);
+            this.textBoxAddinPath.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Файл Excel для обработки";
+            // 
+            // textBoxExcelSheetPath
+            // 
+            this.textBoxExcelSheetPath.Location = new System.Drawing.Point(11, 137);
+            this.textBoxExcelSheetPath.Name = "textBoxExcelSheetPath";
+            this.textBoxExcelSheetPath.Size = new System.Drawing.Size(514, 20);
+            this.textBoxExcelSheetPath.TabIndex = 3;
+            // 
+            // btnSelectExcelFile
+            // 
+            this.btnSelectExcelFile.Location = new System.Drawing.Point(531, 135);
+            this.btnSelectExcelFile.Name = "btnSelectExcelFile";
+            this.btnSelectExcelFile.Size = new System.Drawing.Size(30, 23);
+            this.btnSelectExcelFile.TabIndex = 4;
+            this.btnSelectExcelFile.Text = "...";
+            this.btnSelectExcelFile.UseVisualStyleBackColor = true;
+            this.btnSelectExcelFile.Click += new System.EventHandler(this.btnSelectExcelFile_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Результат";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(11, 243);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxResult.Size = new System.Drawing.Size(514, 120);
+            this.textBoxResult.TabIndex = 6;
+            // 
+            // btnParserTest
+            // 
+            this.btnParserTest.Location = new System.Drawing.Point(512, 56);
+            this.btnParserTest.Name = "btnParserTest";
+            this.btnParserTest.Size = new System.Drawing.Size(55, 23);
+            this.btnParserTest.TabIndex = 2;
+            this.btnParserTest.Text = "Тест";
+            this.btnParserTest.UseVisualStyleBackColor = true;
+            this.btnParserTest.Click += new System.EventHandler(this.btnParserTest_Click);
+            // 
+            // openExcelFileDialog
+            // 
+            this.openExcelFileDialog.Filter = "Excel files|*.xls;*.xlsx|All files|*.*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Настройка по умолчанию";
+            // 
+            // textBoxActiveParserName
+            // 
+            this.textBoxActiveParserName.Location = new System.Drawing.Point(11, 98);
+            this.textBoxActiveParserName.Name = "textBoxActiveParserName";
+            this.textBoxActiveParserName.Size = new System.Drawing.Size(492, 20);
+            this.textBoxActiveParserName.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 373);
+            this.Controls.Add(this.textBoxActiveParserName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxAddinPath);
+            this.Controls.Add(this.btnParserTest);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxExcelSheetPath);
+            this.Controls.Add(this.btnSelectExcelFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.textBoxResult);
+            this.Name = "Form1";
+            this.Text = "FormMain";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxAddinPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxExcelSheetPath;
+        private System.Windows.Forms.Button btnSelectExcelFile;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button btnParserTest;
+        private System.Windows.Forms.OpenFileDialog openExcelFileDialog;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxActiveParserName;
+    }
+}
+
---
+++ WindowsFormsAppTestExcel/Form1.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsAppTestExcel
+{
+    public partial class Form1 : Form
+    {
+        //object[,] arrData; //объявляем двумерный массив (строки и столбцы)
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //имя Excel файла  
+            string fileNameAddin = textBoxAddinPath.Text;
+            string fileNameExcelWorkbook = textBoxExcelSheetPath.Text;
+
+            Cursor savedCursor = this.Cursor;
+
+            // Application
+            Excel.Application xlApp = new Excel.Application();
+
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+
+                // открываем Excel файлы
+                Excel.Workbook xlWb1 = xlApp.Workbooks.Open(fileNameAddin);
+                Excel.Workbook xlWb2 = xlApp.Workbooks.Open(fileNameExcelWorkbook);
+
+                Excel.Worksheet xlSht = xlWb2.Sheets["Лист1"]; //имя листа в файле
+                xlApp.Visible = true;
+                xlApp.Run(@"ParserAddinTest");
+                string activeParserName = textBoxActiveParserName.Text;
+                var res = xlApp.Run(@"StartParser", activeParserName);
+                if (!String.IsNullOrEmpty(res))
+                {
+                    throw new Exception(res);
+                }
+
+                int iLastRow = xlSht.Cells[xlSht.Rows.Count, "B"].End[Excel.XlDirection.xlUp].Row;  //последняя заполненная строка в столбце А
+                var arrData = (object[,])xlSht.Range["A1:B" + iLastRow].Value; //берём данные со 2-й строки, если нужно с 1-й, то замените A2 на A1
+                xlWb2.Close(false); //закрываем файл и сохраняем изменения, если не сохранять, то false                
+
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 1; i <= arrData.GetUpperBound(0); i++) //заполняем ComboBox данными из массива
+                {
+                    //this.comboBox1.Items.Add(arrData[i, 1]);
+                    sb.Append(arrData[i, 2]);
+                    sb.Append("\r\n");
+                }
+                textBoxResult.Text = sb.ToString();
+            }
+            catch (Exception exc)
+            {
+                textBoxResult.Text = exc.Message;
+            }
+            finally
+            {
+                xlApp.Quit(); //закрываем Excel
+                this.Cursor = savedCursor;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = LoadParserPath();
+            textBoxAddinPath.Text = path;
+
+            textBoxActiveParserName.Text = LoadParserDefaultSetup();
+        }
+
+        private string LoadParserPath()
+        {
+            const string regRoot = @"HKEY_CURRENT_USER\Software\VB and VBA Program Settings\Parser\Setup";
+            string s = (string)Registry.GetValue(regRoot, "AddinPath", "") ?? "";
+
+            return s;
+        }
+
+        private string LoadParserDefaultSetup()
+        {
+            const string regRoot = @"HKEY_CURRENT_USER\Software\VB and VBA Program Settings\Parser\Settings";
+            string s = (string)Registry.GetValue(regRoot, "ACTIVE_PARSER", "") ?? "";
+
+            return s;
+
+        }
+
+        private void btnParserTest_Click(object sender, EventArgs e)
+        {
+            string fileNameParser = textBoxAddinPath.Text;
+            Cursor savedCursor = this.Cursor;
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+                textBoxResult.Text = "Parser test starting. Please wait...";
+                // Application
+                Excel.Application xlApp = new Excel.Application();
+
+                // открываем Excel файл
+                Excel.Workbook xlWb1 = xlApp.Workbooks.Open(fileNameParser);
+
+                xlApp.Run(@"ParserAddinTest");
+                xlApp.Quit(); //закрываем Excel
+                xlApp = null;
+                textBoxResult.Text = "Parser test ok.";
+            }
+            catch (Exception exc)
+            {
+                textBoxResult.Text = exc.Message;
+            }
+            finally
+            {
+                this.Cursor = savedCursor;
+            }
+        }
+
+        private void btnSelectExcelFile_Click(object sender, EventArgs e)
+        {
+            if (openExcelFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxExcelSheetPath.Text = openExcelFileDialog.FileName;
+            }
+        }
+    }
+}
